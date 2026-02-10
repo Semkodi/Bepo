a5dd0d49470974813405dc316815812713433b2b
gahom79170@wivstore.com <gahom79170@wivstore.com>
2026-02-10 12:08:47 +0100

    Update vite config base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Ersetze 'Bepo' durch deinen exakten GitHub-Repository-Namen
-  base: './',
+  base: '/Bepo/',
 })
